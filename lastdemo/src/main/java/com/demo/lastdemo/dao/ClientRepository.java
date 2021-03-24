5efc38a1d72fcf490a21fe4f4c52b87786398e60
kkm8257 <tenman2002@gmail.com>
2021-03-25 00:54:14 +0900

    로그인시 유저 있음까지 확인

--- lastdemo/src/main/java/com/demo/lastdemo/dao/ClientRepository.java
+++ lastdemo/src/main/java/com/demo/lastdemo/dao/ClientRepository.java
@@ -2,17 +2,18 @@ package com.demo.lastdemo.dao;
 
 
 import com.demo.lastdemo.vo.UserTable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface ClientRepository extends CrudRepository<UserTable,String> {
+public interface ClientRepository extends JpaRepository<UserTable,String> {
     //id 는 pk의 형식
 
     Optional<UserTable> findByUsername(String username);
-
+    UserTable getOne(String username);
 //    Optional<UserTable> findByUsernameAnduAndUserpwd(String username, String userpwd);
 
 }
--- lastdemo/src/main/java/com/demo/lastdemo/service/ClientLoginService.java
+++ lastdemo/src/main/java/com/demo/lastdemo/service/ClientLoginService.java
@@ -6,6 +6,7 @@ import com.demo.lastdemo.vo.UserTable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -30,27 +31,34 @@ public class ClientLoginService implements UserDetailsService {
         System.out.println("login try..");
         List<GrantedAuthority> authorities=new ArrayList<>();
 
-        if (username.equals("admin")) {
-            System.out.println("admin chk");
-            authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
-        } else {
-            System.out.println("user chk");
-            authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-        }
 
 
-        Optional<UserTable> voName = clientRepository.findByUsername(username);
-        if(voName!=null){
+
+//        Optional<UserTable> voName = clientRepository.findByUsername(username);
+            UserTable vo = clientRepository.getOne(username);
+
+        if(vo!=null){
             System.out.println("유저있음");
             //이름으로 유저 가져오긴했는데 비밀번호는 어떻게 가져다가 비교할지 고민
+            if (username.equals("admin")) {
+                System.out.println("admin chk");
+                authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+            }else {
+                System.out.println("user chk");
+                authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+            }
+
+            return new User(vo.getUsername(),vo.getUserpwd(),authorities);
+
         }else{
             System.out.println("유저없음");
+            return new User(vo.getUsername(),vo.getUserpwd(),authorities);
         }
 //        authorities.add(new SimpleGrantedAuthority("ROLE_USER")); //앞에 ROLE_을 붙여야 home.html에서 정상적으로 hasRole()구문을 인식
 
 
 
-        return null;
+
     }
 
 }
